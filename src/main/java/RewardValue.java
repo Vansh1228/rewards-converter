94a9418340be4461e48128c2b819d89dceecae39
Vansh1228 <guptavansh1228@gmail.com>
2024-01-26 15:32:16 +0530

    modified test files

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,38 @@
 public class RewardValue {
     double cash;
     int miles;
+      public static final double exchangeRate = 0.0035;
 
     RewardValue(double cash){
+
         this.cash = cash;
     }
 
     RewardValue(int miles){
+
         this.miles = miles;
     }
 
+
+    int convertCashValueToMiles(double cashVal){
+        miles = (int)(cashVal/exchangeRate);
+        return miles;
+    }
+
+    double convertMilesValueToCash(int miles){
+        cash = miles*exchangeRate;
+    return cash;
+    }
+
    public int getMilesValue(){
-       return miles = (int)(0.0035*cash);
+//        return miles;
+      return convertCashValueToMiles(cash);
+
     }
 
     public double getCashValue(){
-       return cash;
+//        return cash;
+        return convertMilesValueToCash(miles);
     }
 
 
